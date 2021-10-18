d90bce8e2383a003fbcfb04acd743d4ccab7bfca
Pavel Popeskul <peterjj91@gmail.com>
2021-10-18 23:20:59 +0300

    feature/6/email: NotificationMailer testing for counple of admins

--- spec/mailers/notification_mailer_spec.rb
+++ spec/mailers/notification_mailer_spec.rb
@@ -4,18 +4,19 @@ require 'rails_helper'
 
 RSpec.describe NotificationMailer, type: :mailer do
   describe 'event_created' do
-    let(:user)    { create(:user, admin: true) }
-    let(:event)   { create(:event) }
-    let(:mail)    { NotificationMailer.event_created(user, event) }
+    let(:users) { create_list(:user, 2, admin: true) }
+    let(:event) { create(:event) }
 
     it 'renders the headers' do
-      expect(mail.subject).to eq t('notification_mailer.event_created.subject')
-      expect(mail.to).to eq([user.email])
-      expect(mail.from).to eq(['me@qustom-ror.org'])
-    end
+      users.each do |user|
+        mail = NotificationMailer.event_created(user, event)
 
-    it 'renders the body' do
-      expect(mail.body.encoded).to match(event.title)
+        expect(mail.subject).to eq t('notification_mailer.event_created.subject')
+        expect(mail.to).to eq([user.email])
+        expect(mail.to).to eq([user.email])
+        expect(mail.from).to eq(['me@qustom-ror.org'])
+        expect(mail.body.encoded).to match(event.title)
+      end
     end
   end
 end
